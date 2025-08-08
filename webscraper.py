cf094c7e4ac25238c95d4c9611ccd0ad2ceddfd9
Iago-RM <iagomahiques@gmail.com>
2025-08-08 10:27:10 -0300

    Add files via upload

---
+++ webscraper.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+@app.route("/<name>")
+def hello_name(name):
+    return "hello " + name
+
+if __name__ == "__main__":
+    app.run(debug=True)
