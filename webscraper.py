e0b01c1d304937ea15e48c80f29410d890874361
Iago-RM <iagomahiques@gmail.com>
2025-08-08 11:21:33 -0300

    Add files via upload

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
+
+response = requests.get('https://www.geeksforgeeks.org/python/python-programming-language-tutorial/')
+soup = BeautifulSoup(response.content, 'html.parser')
+content_div = soup.find('div', class_='article--viewer_content')
+content = content_div.find_all('p')
 
 @app.route("/")
 def hello():
-    return "hello world"
-
-@app.route("/<name>")
-def hello_name(name):
-    return "hello " + name
+    if content_div:
+        return render_template("index.html", paras = content)
+    else:
+        return "no article found"
 
 if __name__ == "__main__":
     app.run(debug=True)
